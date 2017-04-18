cea8048cdd66b8ec840dde0721f1a501345799ab
root <root@ns376345.ip-37-187-163.eu>
2017-04-18 15:26:40 +0200

    Branch1 new featrue

---
+++ hello1_new_feature.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+   printf("Hello, woldr!\n");
+   printf("Good Bye! \n");
+   printf("This is branch1 \n")
+   printf("This is new feature in branch1 \n")
+   return 0;
+}
