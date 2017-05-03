ad8b317f6743dbee903d26107f98e5964f4a3738
mhassan <m.hassan_102@yahoo.com>
2017-05-03 13:56:13 +0500

    New change

--- hello1_new_feature.c
+++ hello1_new_feature.c
@@ -8,5 +8,7 @@ int main(int argc, char** argv) {
    printf("This is branch1 \n")
    printf("This is new feature in branch1 \n")
    printf("One more new feature \n")
+    /*New changes in branch1 with Dated 3-5-2017*/
+   printf("New changes in Branch1 with Dated: 3-5-2017")
    return 0;
 }
